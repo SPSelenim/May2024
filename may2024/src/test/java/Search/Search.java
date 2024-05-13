a5daeba1e0e76bc5e861e9c74deb69e1b4dd210a
spselenim <pawal.satish@gmail.com>
2024-05-13 17:21:21 +0530

    all

---
+++ may2024/src/test/java/Search/Search.java
@@ -0,0 +1,51 @@
+package Search;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+//13-May-2024
+//16:11
+
+//corrected all 16:17
+
+
+public class Search {
+
+	public WebDriver driver=null;
+	
+	@BeforeClass
+	public void tearTest()
+	{
+		WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver();
+		  try
+		  {
+		  driver.get("D:\\SeleniumAutomation\\05DemoApps\\demo.html");
+			  driver.manage().window().maximize();
+		  Thread.sleep(2000);
+		  }catch(Exception e)
+		  {
+			  System.out.println(e.getMessage());
+		  }
+		  	
+	}
+	
+	@Test
+  public void f() {
+	  System.out.println("Home : Test1 : 16:77");
+  POM_Search home=new POM_Search(driver);
+  home.getDetails();
+	  
+  }
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
