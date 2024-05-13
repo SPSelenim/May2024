931ed875d59f0e33c9601d37b5c52398e14da1c2
spselenim <pawal.satish@gmail.com>
2024-05-13 16:04:55 +0530

    first commit

---
+++ may2024/src/test/java/Home/NewTest1.java
@@ -0,0 +1,10 @@
+package Home;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  System.out.println("Home : Test1");
+  }
+}
---
+++ may2024/src/test/java/Search/NewTest1.java
@@ -0,0 +1,10 @@
+package Search;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  System.out.println("Search : Test1");
+  }
+}
